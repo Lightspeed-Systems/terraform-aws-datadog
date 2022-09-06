442ec97f93f1a979a1f9073a8265bc8c02dd3d32
Chris Newkirk <chris@lightspeedsystems.com>
2022-09-06 13:20:44 -0700

    better tags

--- locals.tf
+++ locals.tf
@@ -1,8 +1,9 @@
 locals {
   stack_prefix = var.env == "" ? "" : "${join("-", compact([var.namespace, var.env]))}-"
   default_tags = {
-    env       = var.env
-    namespace = var.namespace
-    terraform = "true"
+    env             = var.env
+    namespace       = var.namespace
+    TerraformModule = "terraform-aws-datadog"
+    Environment     = title(var.env)
   }
 }
